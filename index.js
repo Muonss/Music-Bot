f7fcb41ba1678d9b79cd40ed53aac2f2774fab8d
Michael Thomas <142217949+Muonss@users.noreply.github.com>
2023-09-04 21:28:01 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+const { Player } = require('discord-player')
+
+require('dotenv').config()
+
+global.client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+client.config = require('./config')
+client.error = require('./error')
+
+global.player = new Player(client, client.config.opt.discordPlayer)
+
+//require('./src/playerEvents')
+require('./src/loader')
+
+client.login(client.token)
